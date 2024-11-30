55bd8a44fe29c4f328b2096b34a4d7858ac4bd42
Arne Hansen <arne96.hansen@gmail.com>
2024-11-30 10:38:26 +0100

    disable "active_eye = LEFT"

--- paradigm/dynamic_matsvanes/EL_Calibrate.m
+++ paradigm/dynamic_matsvanes/EL_Calibrate.m
@@ -11,7 +11,7 @@ try
     Eyelink('Command', 'saccade_velocity_threshold = 35');
     Eyelink('Command', 'saccade_acceleration_threshold = 9500');
     Eyelink('Command', 'link_sample_data  = LEFT,RIGHT,GAZE,AREA');
-    Eyelink('Command', 'active_eye = LEFT');
+    %Eyelink('Command', 'active_eye = LEFT');
     Eyelink('Command', 'calibration_type = HV9');
     Eyelink('Command', 'enable_automatic_calibration = YES');
     Eyelink('Command', 'automatic_calibration_pacing = 500');
