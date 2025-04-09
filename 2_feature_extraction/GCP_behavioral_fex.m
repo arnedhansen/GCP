ac148219c57b7fce1a4f7bbbb13b8645d2e7a304
Arne Hansen <arne96.hansen@gmail.com>
2025-04-09 14:30:02 +0200

    behavioral FEX 4 contrasts

--- 2_feature_extraction/GCP_behavioral_fex.m
+++ 2_feature_extraction/GCP_behavioral_fex.m
@@ -48,23 +48,32 @@ for subj = 1:length(subjects)
         'Accuracy', num2cell(accuracy), 'ReactionTime', num2cell(reaction_time));
 
     %% Calculate subject-specific data by condition (GazeDev, PupilSize, MSRate)
-    lc = subj_data_behav_trial(ismember([subj_data_behav_trial.Condition], 1));
-    lc_acc = sum([lc.Accuracy])/length(lc)*100;
-    % valid_reactions = ~isnan([lc.ReactionTime]); % Logical array of non-NaN ReactionTime
-    % correct_responses = [lc.Accuracy] == 1; % Logical array of Accuracy == 1
-    % lc_acc = sum(valid_reactions & correct_responses) / sum(valid_reactions) * 100; % Percentage of valid and correct instances
-    lc_rt = mean([lc.ReactionTime], 'omitnan'); 
+    c25 = subj_data_behav_trial(ismember([subj_data_behav_trial.Condition], 1));
+    c25_acc = sum([c25.Accuracy])/length(c25)*100;
+    % valid_reactions = ~isnan([c25.ReactionTime]); % Logical array of non-NaN ReactionTime
+    % correct_responses = [c25.Accuracy] == 1; % Logical array of Accuracy == 1
+    % c25_acc = sum(valid_reactions & correct_responses) / sum(valid_reactions) * 100; % Percentage of valid and correct instances
+    c25_rt = mean([c25.ReactionTime], 'omitnan'); 
 
-    hc = subj_data_behav_trial(ismember([subj_data_behav_trial.Condition], 2));
-    hc_acc = sum([hc.Accuracy])/length(hc)*100;
-    % valid_reactions = ~isnan([hc.ReactionTime]); % Logical array of non-NaN ReactionTime
-    % correct_responses = [hc.Accuracy] == 1; % Logical array of Accuracy == 1
-    % hc_acc = sum(valid_reactions & correct_responses) / sum(valid_reactions) * 100; % Percentage of valid and correct instances
-    hc_rt = mean([hc.ReactionTime], 'omitnan');
+    c50 = subj_data_behav_trial(ismember([subj_data_behav_trial.Condition], 2));
+    c50_acc = sum([c50.Accuracy])/length(c50)*100;
+    % valid_reactions = ~isnan([c50.ReactionTime]); % Logical array of non-NaN ReactionTime
+    % correct_responses = [c50.Accuracy] == 1; % Logical array of Accuracy == 1
+    % c50_acc = sum(valid_reactions & correct_responses) / sum(valid_reactions) * 100; % Percentage of valid and correct instances
+    c50_rt = mean([c50.ReactionTime], 'omitnan');
+
+    c75 = subj_data_behav_trial(ismember([subj_data_behav_trial.Condition], 3));
+    c75_acc = sum([c75.Accuracy])/length(c75)*100;
+    c75_rt = mean([c75.ReactionTime], 'omitnan');
+
+    c100 = subj_data_behav_trial(ismember([subj_data_behav_trial.Condition], 4));
+    c100_acc = sum([c100.Accuracy])/length(c100)*100;
+    c100_rt = mean([c100.ReactionTime], 'omitnan');
 
     %% Create across condition structure
     subject_id = str2num(subjects{subj});
-    subj_data_behav = struct('ID', subject_id, 'Condition', num2cell([1; 2]), 'Accuracy', num2cell([lc_acc; hc_acc]), 'ReactionTime', num2cell([lc_rt; hc_rt]));
+    subj_data_behav = struct('ID', subject_id, 'Condition', num2cell([1; 2; 3 ; 4]), ...
+        'Accuracy', num2cell([c25_acc; c50_acc; c75_acc; c100_acc]), 'ReactionTime', num2cell([c25_rt; c50_rt; c75_rt; c100_rt]));
 
     %% Save
     savepath = strcat('/Volumes/methlab/Students/Arne/GCP/data/features/',subjects{subj}, '/behavioral/');
@@ -72,8 +81,8 @@ for subj = 1:length(subjects)
     cd(savepath)
     save behavioral_matrix_trial subj_data_behav_trial
     save gaze_matrix_subj subj_data_behav
-    save acc lc_acc hc_acc
-    save rt lc_rt hc_rt
+    save acc c25_acc c50_acc c75_acc c100_acc
+    save rt c25_rt c50_rt c75_rt c100_rt
     clc
     disp(['Subject ' num2str(subj) '/' num2str(length(subjects)) ' behavioral feature extraction done.'])
 
